18c07e6040975ac317ace07713bc99a2dd997be3
Pedro Rocha <pedro.rocha@mercadolibre.com>
2022-11-01 17:28:31 -0300

    agregando ejercicio 1

---
+++ sql/clase_01/ej_1.sql
@@ -0,0 +1,27 @@
+SELECT * FROM movies;
+
+SELECT first_name, last_name, rating FROM actors;
+
+SELECT title AS titulo FROM series;
+
+SELECT first_name, last_name FROM actors WHERE rating > 7.5;
+
+SELECT title, rating, awards FROM movies WHERE rating > 7.5 AND awards > 
+2;
+
+SELECT title, rating FROM movies ORDER BY rating;
+
+SELECT title FROM movies LIMIT 3;
+
+SELECT * FROM movies ORDER BY rating DESC LIMIT 5;
+
+SELECT * FROM actors LIMIT 10;
+
+SELECT title, rating FROM movies WHERE title = "Toy Story";
+
+SELECT * FROM actors WHERE first_name LIKE "Sam%";
+
+SELECT title FROM movies WHERE YEAR(release_date) BETWEEN 2004 AND 2008;
+
+SELECT title FROM movies WHERE rating > 3 AND awards > 1 AND 
+YEAR(release_date) BETWEEN 1988 AND 2009;
